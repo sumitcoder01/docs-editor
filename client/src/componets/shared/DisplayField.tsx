1c12d9be78944333443c1c17e0d778651a745ebf
sumitcoder01 <135352466+sumitcoder01@users.noreply.github.com>
2024-05-11 00:42:09 +0530

    style : profile card styles fixed and icons added

--- client/src/componets/shared/DisplayField.tsx
+++ client/src/componets/shared/DisplayField.tsx
@@ -1,11 +1,13 @@
 export type DisplayFieldProps = {
     value: string | null | undefined;
     name: string;
+    IconElement: () => JSX.Element;
 }
 
-export const DisplayField = ({ value, name }: DisplayFieldProps) => {
+export const DisplayField = ({ value, name, IconElement }: DisplayFieldProps) => {
     return (
-        <div className="flex gap-2 my-2 md:mx-2 text-base md:text-md">
+        <div className="flex gap-2 my-2 md:mx-2 text-base md:text-md items-center">
+            <IconElement />
             <span className="font-bold">{name}</span>
             <span>{value ? value : "not specified"}</span>
         </div>
--- client/src/componets/specific/ProfileCard.tsx
+++ client/src/componets/specific/ProfileCard.tsx
@@ -1,17 +1,20 @@
 import { useAuth } from "../../context/authContext";
 import { DisplayField } from "../shared/DisplayField";
+import { UserIcon } from "../icons/UserIcon";
+import { EmailIcon } from "../icons/EmailIcon";
+import { DateIcon } from "../icons/DateIcon";
 
 export const ProfileCard = () => {
     const { user } = useAuth();
     return (
-        <div className="p-4 mt-3 rounded shadow-md w-[16rem] md:w-[40rem] bg-[#e9ecef]">
+        <div className="py-4 px-1 md:px-4 mt-3 rounded shadow-md w-[17rem] md:w-[40rem] bg-[#e9ecef]">
             <h2 className="text-lg font-semibold mb-2">User Profile</h2>
-            <hr className="h-0.5 bg-[#dee2e6]"/>
+            <hr className="h-0.5 bg-[#dee2e6]" />
             <div className="flex flex-col gap-2">
-                <DisplayField name={"Name:"} value={user?.name}/>
-                <DisplayField name={"Email:"} value={user?.email}/>
-                <DisplayField name={"Created At:"} value={user?.createdAt}/>
-                <DisplayField name={"Last Modified At:"} value={user?.updatedAt}/>
+                <DisplayField name={"Name:"} value={user?.name} IconElement={UserIcon} />
+                <DisplayField name={"Email:"} value={user?.email} IconElement={EmailIcon} />
+                <DisplayField name={"Created At:"} value={user?.createdAt} IconElement={DateIcon} />
+                <DisplayField name={"Last Modified At:"} value={user?.updatedAt} IconElement={DateIcon} />
             </div>
         </div>
     );
